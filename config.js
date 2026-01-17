80ac090287a9a529c65043b425fdabd31431f051
Maliya-bro <darkmaliya10@gmail.com>
2026-01-17 07:33:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,9 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "7g4SDZzB#M6RZu7uB_lulaI_ve4VUb7rgv9cE5qEpkqmnh9DrY2U",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/a1b18d21-fd72-43cb-936b-5b9712fb9af0.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD BOT Is Alive Now😍*",
-BOT_OWNER: '94701369636',  // Replace with the owner's phone number
-
+BOT_OWNER: '94701369636',
+AUTO_STATUS_SEEN: 'true',
+AUTO_STATUS_REACT: 'true',
 
 
 };
