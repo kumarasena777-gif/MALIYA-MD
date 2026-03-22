d5a5530505333d3f7fd59b8f24f5832eae9c65b1
kumarasena777-gif <kumarasena777@gmail.com>
2026-03-22 10:00:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ function convertToBool(text, fault = "true") {
 
 module.exports = {
   SESSION_ID:
-    process.env.SESSION_ID || "HxoggBAJ#mdHXec2yZu2Y6kDJZfPRVAwfOoa97nHrGG1_pLjSQ8M", // replace with your session ID
+    process.env.SESSION_ID || "P1RShIrZ#oDNcyj9m64V3fpV7RNWpSxnpsASuHODyzglkpuOdIpA", // replace with your session ID
   ALIVE_IMG:
     process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/WhatsApp%20Image%202026-01-18%20at%2012.37.23.jpeg?raw=true",
  ALIVE_MSG:
